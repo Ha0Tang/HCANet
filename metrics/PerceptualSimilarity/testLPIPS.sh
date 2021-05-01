cce9bde37e693256fac4506452b984b38e787842
Bingoren <31409716+Amazingren@users.noreply.github.com>
2021-05-01 19:43:56 +0200

    Update testLPIPS.sh

--- metrics/PerceptualSimilarity/testLPIPS.sh
+++ metrics/PerceptualSimilarity/testLPIPS.sh
@@ -1,6 +1,6 @@
 CUDA_VISIBLE_DECIVES=1 python lpips_2dirs.py \
--d0 /home/ht1/Xing_tryon/result/TOM/test/try-on \
--d1 /home/ht1/Xing_tryon/data/test/image \
+-d0 PATH_of_your_project/XingVTON/result/TOM/test/try-on \
+-d1 PATH_of_your_project/XingVTON/data/test/image \
 -o ./example_dists.txt \
 --use_gpu
 
